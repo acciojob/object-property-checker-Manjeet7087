baffe5b327c5b453361b75d48924fe94427c0da6
Manjeet7087 <131547570+Manjeet7087@users.noreply.github.com>
2023-06-09 16:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+function hasKey(key) {
+  return key in sampleObject;
+}
+
+console.log(hasKey("red"));    // true
+console.log(hasKey("blue"));   // false
+console.log(hasKey("white"));  // true
+
 }
 
 // Do not change the code below
